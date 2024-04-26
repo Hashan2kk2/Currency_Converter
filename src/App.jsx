b56fe2aa3834f27c215842da3c784ccb305e53e1
HashanGBS <hashan.lakruwan2020@gmail.com>
2024-04-26 15:30:20 +0530

    normal commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-
 import './App.css';
 import 'boxicons'
 import Converter from './Components/Converter';
 
-
 function App() {
 
-  return (
-     <div>
-         <Converter/>
-     </div>
-  )
+    return (
+        <div>
+            <Converter/>
+        </div>
+    )
 }
 
 export default App
