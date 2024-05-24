60298d8c0450ed7be54e4d7e08070af0fd9f9154
Hashan Lakruwan <81254452+Hashan2kk2@users.noreply.github.com>
2024-05-24 12:54:10 +0530

    dark and light theme toggle completed

--- src/Components/Converter.jsx
+++ src/Components/Converter.jsx
@@ -1,76 +1,92 @@
 // src/Components/Converter.jsx
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
 import "../Components/Css/Converter.css";
 import "../Components/Css/Fonts.css";
 import CurrencyDropDown from "../Components/CurrencyDropDown.jsx";
-import { BiChevronDown } from "react-icons/bi";
-import { getExchangeRates } from './Services/exchangeRateService.jsx';
+import {BiChevronDown, BiSun, BiMoon} from "react-icons/bi";
+import {getExchangeRates} from './Services/exchangeRateService.jsx';
 
 function Converter() {
-  const [exchangeRates, setExchangeRates] = useState({});
-  const [inputAmount, setInputAmount] = useState(0);
-  const [convertedAmount, setConvertedAmount] = useState(0);
-  const [baseCurrency, setBaseCurrency] = useState('USD');
-  const [targetCurrency, setTargetCurrency] = useState('USD');
+    const [exchangeRates, setExchangeRates] = useState({});
+    const [inputAmount, setInputAmount] = useState(0);
+    const [convertedAmount, setConvertedAmount] = useState(0);
+    const [baseCurrency, setBaseCurrency] = useState('USD');
+    const [targetCurrency, setTargetCurrency] = useState('USD');
+    const [darkMode, setDarkMode] = useState(false);
 
-  useEffect(() => {
-    console.log('Converted Amount:', convertedAmount); // Log the converted amount
-  }, [convertedAmount]);
+    useEffect(() => {
+        console.log('Converted Amount:', convertedAmount); // Log the converted amount
+    }, [convertedAmount]);
 
-  useEffect(() => {
-    getExchangeRates().then(data => {
-      console.log('Exchange Rates:', data.rates); // Log the fetched exchange rates
-      setExchangeRates(data.rates);
-    });
-  }, []);
+    useEffect(() => {
+        getExchangeRates().then(data => {
+            console.log('Exchange Rates:', data.rates); // Log the fetched exchange rates
+            setExchangeRates(data.rates);
+        });
+    }, []);
 
-  const handleConvert = () => {
-    const baseRate = exchangeRates[baseCurrency];
-    const targetRate = exchangeRates[targetCurrency];
-    if (baseRate && targetRate) {
-      const amount = (parseFloat(inputAmount) / parseFloat(baseRate)) * parseFloat(targetRate);
-      console.log('Calculated Amount:', amount); // Log the calculated amount
-      setConvertedAmount(amount);
-    }
-  };
+    const handleConvert = () => {
+        const baseRate = exchangeRates[baseCurrency];
+        const targetRate = exchangeRates[targetCurrency];
+        if (baseRate && targetRate) {
+            const amount = (parseFloat(inputAmount) / parseFloat(baseRate)) * parseFloat(targetRate);
+            console.log('Calculated Amount:', amount); // Log the calculated amount
+            setConvertedAmount(amount);
+        }
+    };
 
-  return (
-      <div className="h-screen w-screen bg-slate-200 flex items-center justify-center">
-        <div className="max-w-[375px] w-full h-auto p-3 border-slate-300 border rounded-md">
-          <h1 className="text-center text-2xl font-semibold text-slate-900">
-            Currency Converter
-          </h1>
-          <div className="flex flex-col justify-between items-center mt-4 gap-y-4">
-            <CurrencyDropDown exchangeRates={exchangeRates} setSelectedCurrency={setBaseCurrency} />
-            <div className="flex items-center justify-between w-full">
-              <input
-                  type="number"
-                  placeholder="Enter Amount"
-                  className="w-full p-2 border-slate-300 bg-slate-200 border rounded-md"
-                  value={inputAmount}
-                  onChange={e => setInputAmount(e.target.value)}
-              />
+    const toggleTheme = () => {
+        setDarkMode(prevDarkMode => !prevDarkMode);
+        console.log(!darkMode); // Log the toggled state
+    };
+
+    return (
+        <div className={`h-screen w-screen flex items-center justify-center ${darkMode ? 'dark' : ''}`}>
+            <button onClick={toggleTheme}
+                    className="absolute top-0 right-0 p-2 dark:bg-slate-600 m-3 rounded dark:text-slate-300 bg-slate-300">
+                {darkMode ? <BiMoon/> : <BiSun/>}
+            </button>
+            <div className="h-screen w-screen dark:bg-slate-800 bg-slate-200 flex items-center justify-center">
+                <div
+                    className="max-w-[375px] dark:border-slate-500 w-full h-auto p-3 border-slate-300 border rounded-md">
+                    <h1 className="text-center text-2xl font-semibold dark:text-slate-300 text-slate-900">
+                        Currency Converter
+                    </h1>
+                    <div className="flex flex-col justify-between items-center mt-4 gap-y-4">
+                        <CurrencyDropDown exchangeRates={exchangeRates} setSelectedCurrency={setBaseCurrency}/>
+                        <div className="flex items-center justify-between w-full">
+                            <input
+                                type="number"
+                                placeholder="Enter Amount"
+                                className="w-full p-2 border-slate-300 bg-slate-200 border rounded-md dark:bg-slate-800 dark:text-slate-300"
+                                value={inputAmount}
+                                onChange={e => setInputAmount(e.target.value)}
+                            />
+                        </div>
+
+                        <div className="bg-gray-400 p-2 rounded-md text-gray-50">
+                            <BiChevronDown/>
+                        </div>
+
+                        <CurrencyDropDown exchangeRates={exchangeRates} setSelectedCurrency={setTargetCurrency}/>
+                        <div className="flex items-center justify-between w-full">
+                            <input
+                                type="number"
+                                placeholder="Converted Amount"
+                                className="w-full p-2 border-slate-300 bg-slate-200 border rounded-md dark:bg-slate-800 dark:text-slate-300"
+                                value={convertedAmount}
+                                readOnly
+                            />
+                        </div>
+                        <button className="bg-slate-400 dark:bg-slate-300 w-full p-2 rounded-md"
+                                onClick={handleConvert}>Convert
+                        </button>
+                    </div>
+                </div>
             </div>
-
-            <div className="bg-gray-400 p-2 rounded-md text-gray-50">
-              <BiChevronDown />
-            </div>
-
-            <CurrencyDropDown exchangeRates={exchangeRates} setSelectedCurrency={setTargetCurrency} />
-            <div className="flex items-center justify-between w-full">
-              <input
-                  type="number"
-                  placeholder="Converted Amount"
-                  className="w-full p-2 border-slate-300 bg-slate-200 border rounded-md"
-                  value={convertedAmount}
-                  readOnly
-              />
-            </div>
-            <button className="bg-slate-600 w-full p-2 rounded-md" onClick={handleConvert}>Convert</button>
-          </div>
         </div>
-      </div>
-  );
+    );
 }
 
 export default Converter;
+
--- src/Components/CurrencyDropDown.jsx
+++ src/Components/CurrencyDropDown.jsx
@@ -40,25 +40,25 @@ function CurrencyDropDown({ exchangeRates, setSelectedCurrency }) {
         <div className="relative inline-block justify-center text-center w-full" ref={dropdownRef}>
             <div>
                 <button type="button" onClick={() => setIsOpen(!isOpen)}
-                        className="h-[38px] text-center outline-0 block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-neutral-500 sm:text-sm sm:leading-6">
+                        className="h-[38px] text-center outline-0 block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-500 sm:text-sm sm:leading-6 dark:text-slate-300">
                     {selectedCurrency}
                 </button>
             </div>
 
             {isOpen && (
                 <div
-                    className="origin-top-right absolute left-1/2 transform -translate-x-1/2 mt-2 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 w-[300px] z-10">
+                    className="p-2 origin-top-right absolute left-1/2 transform -translate-x-1/2 mt-2 rounded-md shadow-lg bg-slate-100 ring-1 ring-black ring-opacity-5 w-[300px] z-10 dark:bg-gray-800">
                     <input
                         type="text"
                         placeholder="Search..."
                         value={searchTerm}
                         onChange={e => setSearchTerm(e.target.value)}
-                        className="w-full px-2 py-1 border-0 ring-2 focus:ring-2 ring-neutral-600 ring-inset rounded-sm focus:ring-neutral-500 focus:ring-inset focus:ring-opacity-50 focus:outline-none"
+                        className="dark:bg-gray-700 dark:ring-neutral-300 dark:text-neutral-300 w-full px-2 py-1 border-0 ring-2 focus:ring-1 ring-slate-300 ring-inset focus:ring-slate-400 focus:ring-inset focus:ring-opacity-50 focus:outline-none rounded-md"
                     />
                     <div className="py-1 h-[300px] overflow-y-auto" role="menu" aria-orientation="vertical"
                          aria-labelledby="options-menu">
                         {currencies.filter(currency => currency.code.toLowerCase().includes(searchTerm.toLowerCase())).map((currency, index) => {
-                            return <div key={index} className="active:bg-blue-700 hover:bg-blue-800 text-center py-1"
+                            return <div key={index} className="dark:text-neutral-300 dark:hover:bg-neutral-300 dark:hover:text-neutral-800 active:bg-slate-700 hover:bg-slate-600 hover:text-white rounded-md text-center py-1"
                                         onClick={() => handleCurrencySelect(currency.code)}>{currency.code}</div>
                         })}
                     </div>
