a556642344dab41d49d7e2002588c31edef52125
Hashan Lakruwan <81254452+Hashan2kk2@users.noreply.github.com>
2024-05-24 13:48:56 +0530

    Currencies.json added to the public directory

--- src/Components/CurrencyDropDown.jsx
+++ src/Components/CurrencyDropDown.jsx
@@ -8,7 +8,7 @@ function CurrencyDropDown({ exchangeRates, setSelectedCurrency }) {
     const [selectedCurrency, setSelectedCurrencyState] = useState('Select the Currency');
 
     useEffect(() => {
-        fetch("public/currencies.json")
+        fetch("./currencies.json")
             .then(response => response.json())
             .then(data => {
                 const dataArray = Object.values(data);
