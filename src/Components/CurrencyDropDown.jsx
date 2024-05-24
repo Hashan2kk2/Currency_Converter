af0d42efb382f2e5ff4ecfb17b2f206413be9c9e
Hashan Lakruwan <81254452+Hashan2kk2@users.noreply.github.com>
2024-05-24 13:53:35 +0530

    Currencies.json added to the public directory 3

--- src/Components/CurrencyDropDown.jsx
+++ src/Components/CurrencyDropDown.jsx
@@ -8,7 +8,7 @@ function CurrencyDropDown({ exchangeRates, setSelectedCurrency }) {
     const [selectedCurrency, setSelectedCurrencyState] = useState('Select the Currency');
 
     useEffect(() => {
-        fetch("https://simple-currency-converter-nine.vercel.app/Currencies.json")
+        fetch("Currencies.json")
             .then(response => response.json())
             .then(data => {
                 const dataArray = Object.values(data);
